adc7c45b854fe0202448729646d40b289b36b31d
Patrik Svensson <patrik@patriksvensson.se>
2018-02-14 19:22:01 +0100

    use binaries from Azure.

--- build.sh
+++ build.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
-cake_version="0.25.0";
-nuget_version="latest";
-dotnet_version="none";
-coreclr=false;
-bootstrap=true;
+cake_version="0.25.0"
+nuget_version="latest"
+dotnet_version="1.1.7"
+coreclr=false
+bootstrap=true
 
 # Fix up the script root.
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+# Get the platform that we're running on.
+uname_out="$(uname -s)"
+case "${uname_out}" in
+    Darwin*)    platform=osx;;
+    *)          platform=linux
+esac
+
 # Make sure that cakeup exist.
-cakeup=$script_dir/cakeup
+cakeup_version="0.2.19"
+cakeup="$script_dir/cakeup-x86_64-v$cakeup_version"
 if [ ! -f "$cakeup" ]; then
     echo "Downloading cakeup..."
-    curl -Lsfo $cakeup https://github.com/patriksvensson/cakeup-example/releases/download/v0.4.0/cakeup
+    curl -Lsfo $cakeup "https://cakeup.blob.core.windows.net/$platform/cakeup-x86_64-v$cakeup_version"
     if [ $? -ne 0 ]; then
         echo "An error occured while downloading cakeup."
         exit 1
